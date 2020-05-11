122c48c527c33bd5f7318f61bc83980b51903ecc
Satyaarth <satyarthchhabra18@gmail.com>
2020-05-11 17:14:21 +0530

    adding profile pic and full name and uploads etc features

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const request =require('request-promise');
-const cheerio =require('cheerio');
+const request = require('request-promise');
+const cheerio = require('cheerio');
 
 (async () => {
-const username ='willsmith';
-const baseUrl =`https://www.instagram.com/${username}
+    const username = 'willsmith';
+    const baseUrl = `https://www.instagram.com/${username}
 `;
-let response = await request(baseUrl);
+    let response = await request(baseUrl);
 
-let $=cheerio.load(response);
-let script= $('script[type="text/javascript"]').eq(3).html();
-let scriptRegex=/window._sharedData = (.+);/g.exec(script);
-let instagramData=JSON.parse(scriptRegex[1]);
-let instaFollower=instagramData.entry_data.ProfilePage[0].graphql.user.edge_followed_by.count;
-console.log("insta followers are "+ instaFollower);
-let instaFollowing=instagramData.entry_data.ProfilePage[0].graphql.user.edge_follow.count;
-console.log('insta following ' + instaFollowing);
+    let $ = cheerio.load(response);
+    let script = $('script[type="text/javascript"]').eq(3).html();
+    let scriptRegex = /window._sharedData = (.+);/g.exec(script);
+    let instagramData = JSON.parse(scriptRegex[1]);
 
-debugger;
+
+    let instaData = {
+        instafollower: instagramData.entry_data.ProfilePage[0].graphql.user.edge_followed_by.count,
+        instafollowing: instagramData.entry_data.ProfilePage[0].graphql.user.edge_follow.count,
+        uploads: instagramData.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.count,
+        full_name: instagramData.entry_data.ProfilePage[0].graphql.user.full_name,
+        picture_url: instagramData.entry_data.ProfilePage[0].graphql.user.profile_pic_url_hd
+    }
+    console.log(instaData);
+
+
+
+    debugger;
 })();
